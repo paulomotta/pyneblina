f6988a6285178b59ce35b809598727660d9f1974
paulo <paulo@localhost>
2021-04-13 22:25:04 -0300

    added the functions to create the matrix and manipulate with get and set

--- neblina_wrapper.c
+++ neblina_wrapper.c
@@ -52,8 +52,8 @@ static void delete(PyObject* self) {
 static void py_vector_delete(PyObject* self) {
     vector_t* vec = (vector_t*)PyCapsule_GetPointer(self, "py_vector_new");
     printf("vec %p\n",vec);
-    printf("vec->value %p\n",&vec->value);
-    free ((void *)&vec->value);
+    printf("vec->value %p\n",&(vec->value));
+//    free ((void *)&(vec->value));
     free ((void *)vec);
 }
 
@@ -156,6 +156,93 @@ static PyObject* py_vec_add(PyObject* self, PyObject* args) {
     return po;
 }
 
+static void py_matrix_delete(PyObject* self) {
+    matrix_t* mat = (matrix_t*)PyCapsule_GetPointer(self, "py_matrix_new");
+    printf("mat %p\n",mat);
+    printf("mat->value %p\n",&(mat->value));
+//    free ((void *)&(mat->value));
+    free ((void *)mat);
+}
+
+
+static PyObject* py_matrix_new(PyObject* self, PyObject* args){
+    int rows;
+    int cols;
+    int data_type;
+    if (!PyArg_ParseTuple(args, "iii", &rows,&cols,&data_type)) return NULL;
+    printf("create %d\n",rows);
+    printf("create %d\n",cols);
+    matrix_t * a = matrix_new(rows, cols, data_type);
+    printf("malloc %p\n",a);
+    PyObject* po = PyCapsule_New((void*)a, "py_matrix_new", py_matrix_delete);
+    printf("capsule_new %p\n",po);
+    return po;
+}
+
+static PyObject* py_matrix_set(PyObject* self, PyObject* args) {
+    
+    PyObject* pf = NULL;
+    int i;
+    int j;
+    double value;
+    if(!PyArg_ParseTuple(args, "Oiid:py_matrix_set", &pf, &i, &j, &value)) return NULL;
+
+    printf("print (%d,%d)\n",i,j);
+    printf("pf %p\n",pf);
+    matrix_t * mat = (matrix_t *)PyCapsule_GetPointer(pf, "py_matrix_new");
+    printf("mat %p\n",mat);
+    mat->value.f[i*mat->ncol + j] = value;
+    printf("%lf\n",mat->value.f[i*mat->ncol + j]);
+    return Py_None;
+}
+
+static PyObject* py_matrix_get(PyObject* self, PyObject* args) {
+    
+    PyObject* pf = NULL;
+    int i;
+    int j;
+    if(!PyArg_ParseTuple(args, "Oii:py_matrix_set", &pf, &i, &j)) return NULL;
+
+    printf("print (%d,%d)\n",i,j);
+    printf("pf %p\n",pf);
+    matrix_t * mat = (matrix_t *)PyCapsule_GetPointer(pf, "py_matrix_new");
+    printf("mat %p\n",mat);
+    printf("%lf\n",mat->value.f[i*mat->ncol + j]);
+    PyObject * result = PyFloat_FromDouble((double)mat->value.f[i*mat->ncol + j]);
+    return result;
+}
+
+
+static PyObject* py_move_matrix_device(PyObject* self, PyObject* args) {
+    
+    PyObject* pf = NULL;
+    if(!PyArg_ParseTuple(args, "O:py_move_matrix_device", &pf)) return NULL;
+
+    printf("pf %p\n",pf);
+    
+    matrix_t * mat = (matrix_t *)PyCapsule_GetPointer(pf, "py_matrix_new");
+    printf("mat %p\n",mat);
+    matreqdev(mat);
+    return Py_None;
+}
+
+static PyObject* py_move_matrix_host(PyObject* self, PyObject* args) {
+    
+    PyObject* pf = NULL;
+    if(!PyArg_ParseTuple(args, "O:py_move_matrix_device", &pf)) return NULL;
+
+    printf("pf %p\n",pf);
+    
+    matrix_t * mat = (matrix_t *)PyCapsule_GetPointer(pf, "py_matrix_new");
+    printf("mat %p\n",mat);
+    int n = (mat->type==T_FLOAT?mat->nrow*mat->ncol:2*mat->nrow*mat->ncol);
+    matrix_t * out = matrix_new(mat->nrow, mat->ncol, mat->type);
+    cl_int status = clEnqueueReadBuffer(clinfo.q, mat->mem, CL_TRUE, 0, n * sizeof (double), out->value.f, 0, NULL, NULL);
+    CLERR
+    PyObject* po = PyCapsule_New((void*)out, "py_matrix_new", py_matrix_delete);
+    return po;
+}
+
 static PyObject* create(PyObject* self, PyObject* args) {
     int n;
     if (!PyArg_ParseTuple(args, "i", &n))
@@ -196,6 +283,11 @@ static PyMethodDef mainMethods[] = {
     {"vector_get", py_vector_get, METH_VARARGS, "vector_get"},
     {"move_vector_device", py_move_vector_device, METH_VARARGS, "move_vector_device"},
     {"move_vector_host", py_move_vector_host, METH_VARARGS, "move_vector_host"},
+    {"matrix_new", py_matrix_new, METH_VARARGS, "matrix_new"},
+    {"matrix_set", py_matrix_set, METH_VARARGS, "matrix_set"},
+    {"matrix_get", py_matrix_get, METH_VARARGS, "matrix_get"},
+    {"move_matrix_device", py_move_matrix_device, METH_VARARGS, "move_matrix_device"},
+    {"move_matrix_host", py_move_matrix_host, METH_VARARGS, "move_matrix_host"},
     {"vec_add", py_vec_add, METH_VARARGS, "vec_add"},
     {NULL, NULL, 0, NULL}
 };
--- test.py
+++ test.py
@@ -12,7 +12,7 @@ from cmath import *
 init_engine()
 
 n = 3;
-vec_f = vector_new(n, 2)
+vec_f = vector_new(n, 2) # 2 -> float
 print(vec_f)
 vec_2 = vector_new(n, 2)
 print(vec_2)
@@ -35,5 +35,22 @@ out = move_vector_host(res)
 print(vector_get(out,0))
 print(vector_get(out,1))
 print(vector_get(out,2))
+
+mat_f = matrix_new(n,n, 2) # 2 -> float
+print(mat_f)
+
+for i in range(n):
+    for j in range(n):
+        matrix_set(mat_f,i,j,2.0)
+
+move_matrix_device(mat_f)
+
+#res = vec_add(vec_f, vec_2)
+
+#out = move_vector_host(res)
+
+for i in range(n):
+    for j in range(n):
+        print(matrix_get(mat_f,i,j))
 
 stop_engine()
