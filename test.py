6cf4fd876f5e3d96965c4cc76288bd7283b5ea1c
paulo <paulo@localhost>
2021-04-18 16:51:10 -0300

    reorganized the tests into individual Python functions

--- test.py
+++ test.py
@@ -1,145 +1,194 @@
+import sys
 from neblina import *
 
+float = 2
+complex = 13
 
-init_engine()
+def test_vec_add():
+    print("test_vec_add")
+    init_engine()
 
-n = 3;
-vec_f = vector_new(n, 2) # 2 -> float
-print(vec_f)
-vec_2 = vector_new(n, 2)
-print(vec_2)
+    n = 3;
+    vec_f = vector_new(n, float)
+    vec_2 = vector_new(n, float)
 
-vector_set(vec_f,0,1.0)
-vector_set(vec_f,1,1.0)
-vector_set(vec_f,2,1.0)
+    for i in range(n):
+        vector_set(vec_f, i, 1.0)
+        vector_set(vec_2, i, 1.0)
 
-vector_set(vec_2,0,1.0)
-vector_set(vec_2,1,1.0)
-vector_set(vec_2,2,1.0)
+    move_vector_device(vec_f)
+    move_vector_device(vec_2)
 
-move_vector_device(vec_f)
-move_vector_device(vec_2)
+    res = vec_add(vec_f, vec_2)
 
-res = vec_add(vec_f, vec_2)
+    out = move_vector_host(res)
 
-out = move_vector_host(res)
+    for i in range(n):
+        print(vector_get(out,i))
 
-print(vector_get(out,0))
-print(vector_get(out,1))
-print(vector_get(out,2))
+    stop_engine()
 
-mat_f = matrix_new(n,n, 2) # 2 -> float
-print(mat_f)
+def test_matrix_multiplication():
+    print("test_matrix_multiplication")
+    init_engine()
 
-for i in range(n):
-    for j in range(n):
-        matrix_set(mat_f,i,j,2.0)
+    n = 3;
+    vec_f = vector_new(n, float)
+    for i in range(n):
+        vector_set(vec_f, i, 1.0)
 
-move_matrix_device(mat_f)
+    mat_f = matrix_new(n,n, float)
 
-#res = vec_add(vec_f, vec_2)
+    for i in range(n):
+        for j in range(n):
+            matrix_set(mat_f, i, j, 2.0)
 
-#out = move_vector_host(res)
+    move_vector_device(vec_f)
+    move_matrix_device(mat_f)
 
-for i in range(n):
-    for j in range(n):
-        print(matrix_get(mat_f,i,j))
+    res = matvec_mul(vec_f, mat_f)
 
-n = 10
-smat_f = sparse_matrix_new(n,n, 2) # 2 -> float
+    out = move_vector_host(res)
 
-sparse_matrix_set_real(smat_f, 0, 0, 3.);
-sparse_matrix_set_real(smat_f, 0, 1, 3.);
-sparse_matrix_set_real(smat_f, 0, 9, 3.);
+    for i in range(n):
+        print(vector_get(out,i))
 
-sparse_matrix_set_real(smat_f, 1, 1, 3.);
-sparse_matrix_set_real(smat_f, 1, 5, 3.);
-sparse_matrix_set_real(smat_f, 1, 8, 3.);
+    stop_engine()
 
-sparse_matrix_set_real(smat_f, 2, 2, 3.);
-sparse_matrix_set_real(smat_f, 2, 4, 3.);
-sparse_matrix_set_real(smat_f, 2, 7, 3.);
+def test_sparse_matrix_multiplication():
+    print("test_sparse_matrix_multiplication")
+    init_engine()
 
-sparse_matrix_set_real(smat_f, 3, 3, 3.);
-sparse_matrix_set_real(smat_f, 3, 1, 3.);
-sparse_matrix_set_real(smat_f, 3, 6, 3.);
+    n = 10
+    vec_f = vector_new(n, float)
+    for i in range(n):
+        vector_set(vec_f, i, 1.0)
 
-sparse_matrix_pack(smat_f)
+    smat_f = sparse_matrix_new(n,n, float)
 
-vec_res = matvec_mul(vec_f,mat_f)
+    sparse_matrix_set_real(smat_f, 0, 0, 3.);
+    sparse_matrix_set_real(smat_f, 0, 1, 3.);
+    sparse_matrix_set_real(smat_f, 0, 9, 3.);
 
-out = move_vector_host(vec_res)
-n=3
-for i in range(n):
-    print(vector_get(out,i))
+    sparse_matrix_set_real(smat_f, 1, 1, 3.);
+    sparse_matrix_set_real(smat_f, 1, 5, 3.);
+    sparse_matrix_set_real(smat_f, 1, 8, 3.);
 
+    sparse_matrix_set_real(smat_f, 2, 2, 3.);
+    sparse_matrix_set_real(smat_f, 2, 4, 3.);
+    sparse_matrix_set_real(smat_f, 2, 7, 3.);
 
-n = 3;
-v1 = vector_new(n, 2) # 2 -> float
-v2 = vector_new(n, 2)
+    sparse_matrix_set_real(smat_f, 3, 3, 3.);
+    sparse_matrix_set_real(smat_f, 3, 1, 3.);
+    sparse_matrix_set_real(smat_f, 3, 6, 3.);
 
-vector_set(v1,0,1.0)
-vector_set(v1,1,1.0)
-vector_set(v1,2,1.0)
+    sparse_matrix_pack(smat_f)
 
-vector_set(v2,0,1.0)
-vector_set(v2,1,1.0)
-vector_set(v2,2,1.0)
+    print(1)
+    move_vector_device(vec_f)
+    print(2)
+    move_sparse_matrix_device(smat_f)
+    print(3)
 
-move_vector_device(v1)
-move_vector_device(v2)
+    res = matvec_mul(smat_f, vec_f)
+    print(4)
 
+    out = move_vector_host(res)
+    print(5)
 
-vec_res = vec_prod(v1,v2)
+    for i in range(n):
+        print(vector_get(out,i))
 
-out = move_vector_host(vec_res)
-n=3
-for i in range(n):
-    print(vector_get(out,i))
+    stop_engine()
 
-n = 4;
-v1 = vector_new(n, 2) # 2 -> float
+def test_vec_conjugate():
+    print("test_vec_conjugate")
+    init_engine()
+    n = 3;
+    v1 = vector_new(n, complex)
 
-vector_set(v1,0,2.0)
-vector_set(v1,1,2.0)
-vector_set(v1,2,2.0)
-vector_set(v1,3,2.0)
+    vector_set(v1, 0, 2.0, 2.0)
+    vector_set(v1, 1, 2.0, 2.0)
+    vector_set(v1, 2, 2.0, 2.0)
 
-move_vector_device(v1)
+    res = vec_conj(v1);
 
-offset=2
-vec_res = vec_add_off(offset, v1)
+    #TODO completar o get/set para complexos
+    for i in range(n*2):
+        print(vector_get(out,2*i))
+        print(vector_get(out,2*i+1))
 
-out = move_vector_host(vec_res)
+    stop_engine()
 
-for i in range(offset):
-    print(vector_get(out,i))
+def test_vec_sum():
+    print("test_vec_sum")
+    init_engine()
+    n = 4;
+    v1 = vector_new(n, float)
 
-n = 4;
-v1 = vector_new(n, 2) # 2 -> float
+    for i in range(n):
+        vector_set(v1, i, 2.0)
 
-vector_set(v1,0,2.0)
-vector_set(v1,1,2.0)
-vector_set(v1,2,2.0)
-vector_set(v1,3,2.0)
+    move_vector_device(v1)
 
-move_vector_device(v1)
+    res = vec_sum(v1)
 
-res = vec_sum(v1)
+    print (res)
 
-print (res)
+    stop_engine()
 
-n = 3;
-v1 = vector_new(n, 13) # 13 -> complex
+def test_vec_add_off():
+    print("test_vec_add_off")
+    init_engine()
+    n = 4;
+    v1 = vector_new(n, 2) # 2 -> float
 
-vector_set(v1,0, 2.0, 2.0)
-vector_set(v1,1, 2.0, 2.0)
-vector_set(v1,2, 2.0, 2.0)
-    
-res = vec_conj(v1);
+    vector_set(v1,0,2.0)
+    vector_set(v1,1,2.0)
+    vector_set(v1,2,2.0)
+    vector_set(v1,3,2.0)
 
-#TODO completar o get/set para complexos
-for i in range(offset):
-    print(vector_get(out,i))
+    move_vector_device(v1)
 
-stop_engine()
+    offset=2
+    vec_res = vec_add_off(offset, v1)
+
+    out = move_vector_host(vec_res)
+
+    for i in range(offset):
+        print(vector_get(out,i))
+
+    stop_engine()
+
+def test_vec_prod():
+    print("test_vec_prod")
+    init_engine()
+    n = 3;
+    v1 = vector_new(n, float)
+    v2 = vector_new(n, float)
+
+    for i in range(n):
+        vector_set(v1, i, 1.0)
+        vector_set(v2, i, 1.0)
+
+    move_vector_device(v1)
+    move_vector_device(v2)
+
+    vec_res = vec_prod(v1,v2)
+
+    out = move_vector_host(vec_res)
+
+    for i in range(n):
+        print(vector_get(out,i))
+
+    stop_engine()
+
+test_vec_add()
+test_matrix_multiplication()
+#test_sparse_matrix_multiplication()
+#test_vec_conjugate()
+test_vec_sum()
+test_vec_add_off()
+test_vec_prod()
+
+sys.exit()
