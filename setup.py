5d172dccc353bf34b23223f268d97e8b2c7b5bcf
paulo <paulo@localhost>
2021-04-21 22:53:08 -0300

    updated the location of neblina-core library

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup, Extension
 neblina_module = Extension('neblina',
                         define_macros = [('MAJOR_VERSION', '0'),
                                          ('MINOR_VERSION', '2')],
-                        include_dirs = ['/home/paulo/Documentos/pesquisa/quantum/neblina-core/include'],
+                        include_dirs = ['../neblina-core/include'],
                         libraries = ['neblina-core'],
-                        library_dirs = ['/home/paulo/Documentos/pesquisa/quantum/neblina-core'],
+                        library_dirs = ['../neblina-core'],
                         sources = ['neblina_wrapper.c'])
 setup(name = 'NeblinaExtension',version='0.2',description = 'This is the neblina math package',ext_modules = [neblina_module])
