be48b04ef38c47ea83a7ddee7cae308c6878d3a2
paulo <prmottajr@gmail.com>
2023-06-25 11:01:10 -0300

    fix: updated the calls to vector_new and matrix_new due to neblina-core fixes. Also updated the setup script

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup, Extension
 neblina_module = Extension('neblina',
                         define_macros = [('MAJOR_VERSION', '0'),
-                                         ('MINOR_VERSION', '2')],
+                                         ('MINOR_VERSION', '3')],
                         include_dirs = ['../neblina-core/include'],
                         libraries = ['neblina-core'],
                         library_dirs = ['../neblina-core'],
                         sources = ['neblina_wrapper.c'])
-setup(name = 'NeblinaExtension',version='0.2',description = 'This is the neblina math package',ext_modules = [neblina_module])
+setup(name = 'NeblinaExtension',version='0.3',description = 'This is the neblina math package',ext_modules = [neblina_module])
