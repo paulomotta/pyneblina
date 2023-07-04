c3f1b7c9492e41c119580b23a7495fe452a7e1a8
paulo <prmottajr@gmail.com>
2023-07-03 23:50:47 -0300

    fix: changed the setup to refer to the system neblina-core library

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ neblina_module = Extension('neblina',
                                          ('MINOR_VERSION', '3')],
                         include_dirs = ['../neblina-core/include'],
                         libraries = ['neblina-core'],
-                        library_dirs = ['../neblina-core'],
+                        library_dirs = ['/usr/local/lib64'],
                         sources = ['neblina_wrapper.c'])
 setup(name = 'NeblinaExtension',version='0.3',description = 'This is the neblina math package',ext_modules = [neblina_module])
